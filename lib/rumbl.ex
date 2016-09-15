98012809e4003de89d45a56d6f65a4ee8eba0603
Rob Cataneo <robcataneo@gmail.com>
2016-09-15 11:29:28 -0700

    Added in memory repository with fake users

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -9,7 +9,7 @@ defmodule Rumbl do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(Rumbl.Repo, []),
+      # supervisor(Rumbl.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
@@ -28,4 +28,30 @@ defmodule Rumbl do
     Rumbl.Endpoint.config_change(changed, removed)
     :ok
   end
+
+end
+
+defmodule Rumbl.Repo do
+
+  @moduledoc """
+  In memory Repository
+  """
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
+
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
+     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
+  end
+  def all(_module), do: []
 end
