b11763bc3da0fba6c7b46dae4aa014367ccfe56b
Rob Cataneo <robcataneo@gmail.com>
2016-09-17 20:02:24 -0700

    Adding an authentication plug to handle session and current user. Restriction for actions for user using  authenticate plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,8 +1,11 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
+
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
   end
 
@@ -10,8 +13,6 @@ defmodule Rumbl.UserController do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
   end
-
-  alias Rumbl.User
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
@@ -23,11 +24,22 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
+  end
 
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
