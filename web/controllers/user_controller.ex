5e7c35b1b5cf2d48c55241527abd78e9ac35a0e9
Rob Cataneo <robcataneo@gmail.com>
2016-09-17 17:45:25 -0700

    Adding new and create actions for user using changesets. Added validations for user

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,7 +7,27 @@ defmodule Rumbl.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-  	user = Repo.get(Rumbl.User, id)
-  	render conn, "show.html", user: user
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
+
+  alias Rumbl.User
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+
   end
 end
