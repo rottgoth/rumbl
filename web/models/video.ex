3214782012ca63ce5d9476650c102d301abb66fa
Rob Cataneo <robcataneo@gmail.com>
2016-09-19 11:18:10 -0700

    Added categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
@@ -18,8 +19,8 @@ defmodule Rumbl.Video do
   #   |> cast(params, [:url, :title, :description])
   #   |> validate_required([:url, :title, :description])
   # end
-  @required_fields ~w(url title description user_id)
-  @optional_fields ~w()
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
 
   def changeset(model, params \\ :empty) do
     model
