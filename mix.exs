e13bd9f2ab1f037f5d88c9d5ed04349087139c29
Rob Cataneo <robcataneo@gmail.com>
2016-09-28 09:06:08 -0700

    Extracted info sys into its own application. Moved rumbl into an elixir umbrella application

--- mix.exs
+++ mix.exs
@@ -4,6 +4,10 @@ defmodule Rumbl.Mixfile do
   def project do
     [app: :rumbl,
      version: "0.0.1",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -19,7 +23,7 @@ defmodule Rumbl.Mixfile do
   def application do
     [mod: {Rumbl, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin]]
+                    :phoenix_ecto, :postgrex, :comeonin, :info_sys]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +42,7 @@ defmodule Rumbl.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:sweet_xml, "~> 0.5.0"},
+     {:info_sys, in_umbrella: true},
      {:comeonin, "~> 2.5"}]
   end
 
